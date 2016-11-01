3d8d2239e1f77bcc71a4d6495823c99b22977f3f
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-11-01 11:55:08 -0400

    Add w5d2 notes and code

---
+++ w5d2-react/form-example/src/index.jsx
@@ -0,0 +1,15 @@
+// Application entrypoint.
+
+// Load up the application styles
+require("../styles/application.scss");
+
+// Render the top-level React component
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+window.render = function() {
+  ReactDOM.render(<App />, document.getElementById('react-root'));
+}
+
+render();
