651d8ba81408df9d43f2cedc76bf9d795ee86867
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-04-23 16:28:23 -0400

    Update w4d2b notes and example

--- w4d2b-knex/example.js
+++ w4d2b-knex/example.js
@@ -1,17 +1,18 @@
 // before you run below examples, run:
-// sqlite3 music.db < seed.sql
+// npm install
+// knex migrate:latest 
+// knex seed:run
 
 var knex = require('knex');
+var config = require('./knexfile.js')
 
-var db = knex({
-   client: 'sqlite3',
-   connection: {
-       filename: './music.db'
-}});
+var db = knex(config.development);
 
-// INSERT (w/ NESTING)
 
-/*
+// -------------------------------------
+
+console.log("INSERT (w/ NESTING)")
+
 db.insert({name: "Artist Name"}).into("artists").then(function(results) {
    db.insert({title: "Album Title", year: 2018, artist_id: results[0]}).into("albums").then(function(results) {
      db.insert({title: "Song Title", number: 5, album_id: results[0]}).into("tracks").then(function() {
@@ -19,11 +20,12 @@ db.insert({name: "Artist Name"}).into("artists").then(function(results) {
      });
    });
 });
-*/
 
-// INSERT (w/ PROMISES)
 
-/*
+// -------------------------------------
+
+console.log("INSERT (w/ PROMISES)")
+
 db.insert({name: "Artist Name"}).into("artists").then(function(results) {
   return db.insert({title: "Album Title", year: 2018, artist_id: results[0]}).into("albums");
 }).then(function(results) {
@@ -33,19 +35,22 @@ db.insert({name: "Artist Name"}).into("artists").then(function(results) {
 }).then(function(results) {
   console.log(results)
 });
-*/
 
-// DELETE
 
-/*
+// -------------------------------------
+
+
+console.log("DELETE")
+
 db.delete().from("tracks").where({title: "Song Title"}).then();
 db.delete().from("albums").where({title: "Album Title"}).then();
 db.delete().from("artists").where({name: "Artist Name"}).then();
-*/
-
-// SELECT name FROM artists;
 
 
+// -------------------------------------
+
+
+console.log("SELECT name FROM artists;");
 
 var query = db.select("name").from("artists");
 
@@ -55,22 +60,21 @@ query.then(function(results) {
    console.log(results);
 });
 
-db.destroy();
+
+// -------------------------------------
 
 
+console.log("SELECT title, number FROM tracks;");
 
-// SELECT title, number FROM tracks;
 
-/*
 db.select("title", "number").from("tracks").then(function(results) {
   console.log(results);
 });
-*/
 
+// -------------------------------------
 
-// INCREMENTAL QUERIES
+console.log("INCREMENTAL QUERIES");
 
-/*
 var params = {"artist": "Daft Punk"};
 var params2 = {"album": "Discovery"};
 
@@ -91,8 +95,48 @@ console.log(query.toSQL().sql);
 query.then(function(results) {
   console.log(results);
 });
-*/
 
 
-// db.destroy();
+// ---------------------------------------
 
+console.log("NESTED QUERIES USING CALLBACKS");
+
+var query = db.select("name", "id").from("artists");
+
+query.asCallback(function(err, results) {
+   results.forEach(function(artist) {
+     let q = db.select("title").from("albums").where({artist_id: artist.id});
+     q.asCallback(function(err,   album){
+       if(err){
+         console.log(err)
+       } else {
+        console.log(album);
+       }
+     });
+   });
+  // db.destroy();
+});
+
+// -------------------------------------------
+
+console.log("NESTED QUERIES USING PROMISES");
+
+var query = db.select("name", "id").from("artists");
+
+console.log(query.toSQL().sql);
+
+query.then(function(results) {
+  return results.map(function(artist) { return artist.id });
+}).then(function(ids) {
+  return Promise.all(
+    ids.map(function(id) {
+      return db.select("title").from("albums").where({artist_id: id});
+    })
+  )
+}).then(function(results) {
+   console.log(results)
+}).then(function() {
+   //db.destroy();
+}).catch(function(err) {
+   console.log(err)
+})
