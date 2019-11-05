0faa1826eeb5cc56c2e90833c57ecfbabe9d43f5
Rafal Dittwald <rafal.dittwald@gmail.com>
2019-11-05 12:35:22 -0500

    Update new-w4d2-demo to make it feature-parity w/ other demo

--- new-w4d2-client-js-and-jquery/demos/v2_html_in_js.js
+++ new-w4d2-client-js-and-jquery/demos/v2_html_in_js.js
@@ -13,27 +13,54 @@ const pages = [
 const currentPageId = pages[0].id;
 
 const escape = function(str) {
-	var div = document.createElement('div');
+  const div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
 const createPage = function(page) {
-	$(`<section data-page-id="${page.id}">
-    	<h1>${escape(page.title)}</h1>
-    	<p>${escape(page.content)}</p>
+  $(`<section data-page-id="${page.id}">
+      <h1>${escape(page.title)}</h1>
+      <p>${escape(page.content)}</p>
     </section>`).appendTo($("body"))
 }
 
 const createPageV2 = function(page) {
-	$section = $("<section>").attr("data-page-id", page.id)
+  $section = $("<section>").attr("data-page-id", page.id)
   $("<h1>").text(page.title).appendTo($section)
   $("<p>").text(page.content).appendTo($section)
-	$section.appendTo($("body"))
+  $section.appendTo($("body"))
+}
+
+const updateTabs = function(pageId) {
+	// make all li inactive, then make current li active
+	$("nav > ul > li").removeClass("active")
+	$("nav > ul > li[data-page-id="+pageId+"]")
+  	.addClass("active")
+
+  // hide all pages, then show current page
+  $("section").remove()
+  createPage(pages.filter(function(page) { return page.id == pageId })[0]);
+}
+
+const createNav = function(pages) {
+  const $nav = $("<nav>")
+  const $ul = $("<ul>").appendTo($nav)
+
+  pages.forEach(function(page) {
+    $("<li>")
+      .attr("data-page-id", page.id)
+      .text(page.title)
+      .on("click", function() {
+        updateTabs(page.id, this)
+      })
+      .appendTo($ul)
+  });
+
+  $("body").append($nav);
 }
 
 $(function() {
-   pages.forEach(function(page) {
-   	  createPage(page)
-   })
+  createNav(pages);
+  updateTabs(currentPageId);
 })
